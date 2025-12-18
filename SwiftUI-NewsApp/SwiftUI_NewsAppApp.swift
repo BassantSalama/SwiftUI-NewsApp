a4f132d843d00083acd5f44a5edd587f692cb257
Basant <basantsalama05@gmail.com>
2025-12-18 05:46:31 +0200

    Initial commit - project created

---
+++ SwiftUI-NewsApp/SwiftUI_NewsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_NewsAppApp.swift
+//  SwiftUI-NewsApp
+//
+//  Created by mac on 18/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_NewsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
